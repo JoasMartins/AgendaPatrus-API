4c4b2c8aa6db3e58f7ad0c2c7f719214c67d2b27
Joás Martins <joasmcarmo@gmail.com>
2024-06-05 16:58:49 -0300

    [1.3.11] Update Student '/students/add'

--- index.js
+++ index.js
@@ -1326,10 +1326,14 @@ api.post("/crypto", async (req, res) => {
 api.get("/crypto", async (req, res) => {
     var dataString = req.query.crypto
     let key = appData.cryptoKey
-    console.log(dataString, key)
+    
 
     let cryptoProcess = crypto.AES.decrypt(dataString, key)
+    console.log(cryptoProcess)
+    let newChange = cryptoProcess.toString()
+    console.log(newChange)
     let cryptoString = cryptoProcess.toString(crypto.enc.Utf8)
+    console.log(cryptoString)
     return res.status(200).json({ cryptoString })
 })
 
